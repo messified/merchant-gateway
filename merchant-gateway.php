6114b2f811b1eb93dd3f80c89063d2b5f157cd2e
messified <jesse.c.reese@gmail.com>
2019-05-20 13:44:16 -0700

    Implement new github updater

--- merchant-gateway.php
+++ merchant-gateway.php
@@ -9,13 +9,13 @@
  * that starts the plugin.
  *
  * @link              https://www.merchantpaymentpro.com
- * @since             1.0.2
+ * @since             1.0.0
  * @package           Merchant_Gateway
  *
  * @wordpress-plugin
  * Plugin Name:       Merchant Gateway
  * Description:		  PayPro Merchant Gateway Integration
- * Version:           1.0.2
+ * Version:           1.0.0
  * Author:            PayPro
  * Author URI:        https://github.com/messified/merchant-gateway-woocommerce
  * License:           GPL-2.0+
@@ -31,10 +31,10 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * Currently plugin version.
- * Start at version 1.0.2 and use SemVer - https://semver.org
+ * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'MERCHANT_GATEWAY_VERSION', '1.0.2' );
+define( 'MERCHANT_GATEWAY_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
@@ -70,7 +70,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-merchant-gateway.php';
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.0.2
+ * @since    1.0.0
  */
 function run_merchant_gateway() {
 
